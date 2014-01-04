4c7139b6972affc3de45099dc980157483f734ed
Dominic Wong <dominic.wong.617@gmail.com>
2014-01-04 16:28:46 +0800

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kairos::Application.config.secret_token = '50520c6ae7b9f2cbcf30089d4d216ff2efa2bd38e0929acafb4510a151c2bb56469e55d75b0ad9d7b184f5b008b1f664b2363f614093c7d2ef278ee309fdb6d6'
