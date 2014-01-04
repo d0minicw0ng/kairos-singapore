8a4467bf41906aabf9ee57cabaec8dcbd3fb2cd2
Dominic Wong <dominic.wong.617@gmail.com>
2014-01-04 23:04:14 +0800

    project model and controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,25 @@
+class ProjectsController < ApplicationController
+	
+	def new
+		@project = Project.new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end  
+
+	def index
+	end
+
+	def show
+		@project = Project.find(params[:id])
+	end
+
+	def destroy
+	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140104144447) do
+ActiveRecord::Schema.define(:version => 20140104145848) do
+
+  create_table "projects", :force => true do |t|
+    t.string   "title",       :null => false
+    t.text     "description", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
