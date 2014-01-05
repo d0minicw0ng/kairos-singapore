d59f0ceb0472b53e1605c6516dfc3cd34801af63
Dominic Wong <dominic.wong.617@gmail.com>
2014-01-05 21:22:34 +0800

    attr_accessible for comment and project (for now until strong parameters)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
+  attr_accessible :user_id, :content, :commentable_id, :commentable_type
   belongs_to :commentable, polymorphic: true
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ActiveRecord::Base
+  attr_accessible :title, :description, :video_url
+  validates_presence_of :title, :description, :video_url
   has_many :comments, as: :commentable
   has_many :project_tags
   has_many :tags, through: :project_tags
