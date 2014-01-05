e830245c66ef94c795b97bda6f143af536780076
Dominic Wong <dominic.wong.617@gmail.com>
2014-01-05 21:08:29 +0800

    Tag model created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140105130251) do
+ActiveRecord::Schema.define(:version => 20140105130647) do
 
   create_table "articles", :force => true do |t|
     t.integer  "user_id",    :null => false
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(:version => 20140105130251) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.string   "video_url"
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name",       :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
