129149ffac42fcc461071793b2d88d9977e21d45
Dominic Wong <dominic.wong.617@gmail.com>
2014-01-11 17:54:18 +0800

    Setting root path just to have something to show for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Kairos::Application.routes.draw do
   devise_for :users
+	devise_scope :user do 
+    authenticated :user do
+      root :to => 'devise/sessions#new'
+    end
+    unauthenticated :user do
+      root :to => 'devise/sessions#new'
+    end
+    root :to => 'devise/sessions#new'
+	end
 end
