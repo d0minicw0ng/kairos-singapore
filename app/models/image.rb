e558a1dad3922ccaf563558f544ba1ccc6e5063d
Dominic Wong <dominic.wong.617@gmail.com>
2014-01-18 18:12:16 +0800

    Paperclip and S3 for Image model

---
+++ app/models/image.rb
@@ -0,0 +1,16 @@
+class Image < ActiveRecord::Base
+	attr_accessible :avatar
+	has_attached_file :avatar, 
+		styles: { 
+			large: '500x500>', 
+			medium: '300x300>',
+		}, 
+		default_url: 'images/missing.png'
+
+	validates_attachment :avatar, 
+		presence: true,
+		content_type: { 
+			content_type: ['image/png', 'image/jpg', 'image/jpeg'] 
+		},
+		size: { :in => 0..2.megabytes }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140105131043) do
+ActiveRecord::Schema.define(:version => 20140118093835) do
 
   create_table "article_tags", :force => true do |t|
     t.integer  "article_id", :null => false
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(:version => 20140105131043) do
     t.string   "commentable_type", :null => false
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+  end
+
+  create_table "images", :force => true do |t|
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "project_tags", :force => true do |t|
