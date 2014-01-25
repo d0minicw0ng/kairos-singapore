c31e0e228937ed57c27e6320fdbbb90f2a5329bc
Dominic Wong <dominic.wong.617@gmail.com>
2014-01-25 15:22:15 +0800

    Working on image upload, but maybe multiple is nicer?

---
+++ app/assets/javascripts/projects.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  $('.upload-image-button').click(function(e) {
+    e.preventDefault();
+    $('#project_images_attributes_avatar').click();
+  });
+
+  $('#project_images_attributes_avatar').change(function() {
+    $('#upload-image').val($(this).val());
+  });
+
+});
