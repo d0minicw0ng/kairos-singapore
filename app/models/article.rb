0e2f9b18acd453264422c79aef6c046cc55faba3
Dominic Wong <dominic.wong.617@gmail.com>
2014-01-27 22:08:08 +0800

    slug for article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,11 @@
 class Article < ActiveRecord::Base
-  attr_accessible :body, :title, :user_id
+  attr_accessible :title, :body, :user_id
   validates_presence_of :title, :body, :user_id
   belongs_to :user
   has_many :comments, as: :commentable
   has_many :article_tags
   has_many :tags, through: :article_tags
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140125100522) do
+ActiveRecord::Schema.define(:version => 20140127140212) do
 
   create_table "article_tags", :force => true do |t|
     t.integer  "article_id", :null => false
@@ -26,7 +26,10 @@ ActiveRecord::Schema.define(:version => 20140125100522) do
     t.text     "body",       :null => false
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "slug"
   end
+
+  add_index "articles", ["slug"], :name => "index_articles_on_slug", :unique => true
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id",          :null => false
