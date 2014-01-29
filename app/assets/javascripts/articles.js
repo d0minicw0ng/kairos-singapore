044251968e4a290da96ce90f934fb0513243e13e
Dominic Wong <dominic.wong.617@gmail.com>
2014-01-29 15:29:01 +0800

    articles.js no erb

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
 
   // Append new comment to the list upon AJAX success
   $('.new_comment').on('ajax:success', function(e, comment) {
-    var listItem = HandlebarsTemplates['comments/show']({ comment: comment });
+    var listItem = HandlebarsTemplates['comments/show']({ 
+      comment: comment
+    });
     $('ul.comments').append(listItem);
     $('input#comment_content').val('');
   });
