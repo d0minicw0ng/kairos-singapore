73627f303ba8ab3addc2bd73cb71b312cc55d46e
Dominic Wong <dominic.wong.617@gmail.com>
2014-01-29 14:05:00 +0800

    It works

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
-  $('.new_comment').on('ajax:success', function(e, data) {
-    console.log(e);
-    console.log(data);
-    alert('Comment Left')
+
+  // Append new comment to the list upon AJAX success
+  $('.new_comment').on('ajax:success', function(e, comment) {
+    var commentId = comment.id;
+    var listItem = $('<li/>').addClass('comment').attr('id', 'comment_' + commentId).text(comment.content);
+    $('ul.comments').append(listItem);
+    $('input#comment_content').val('');
   });
+  
 });
