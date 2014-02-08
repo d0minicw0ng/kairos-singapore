31f8094cf39b7962f0cc84eac4dd2f25458fde1c
Dominic Wong <dominic.wong.617@gmail.com>
2014-02-08 18:06:19 +0800

    application key in yml

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Kairos::Application.config.secret_token = '50520c6ae7b9f2cbcf30089d4d216ff2efa2bd38e0929acafb4510a151c2bb56469e55d75b0ad9d7b184f5b008b1f664b2363f614093c7d2ef278ee309fdb6d6'
+Kairos::Application.config.secret_token = ENV['APPLICATION_KEY']
