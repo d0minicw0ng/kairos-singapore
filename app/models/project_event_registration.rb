91aeaf6a644d1162a1966dffd18b115039233591
Dominic Wong <dominic.wong.617@gmail.com>
2014-02-23 22:23:33 +0800

    WIP - mechanism to get accepted project for each event

--- app/models/project_event_registration.rb
+++ app/models/project_event_registration.rb
@@ -7,7 +7,51 @@ class ProjectEventRegistration < ActiveRecord::Base
 
   has_many :votes
 
-  def votes_count
-    votes.count
+  def self.registration_ids_for_event(event_id)
+     where(event_id: event_id).pluck(:id)
+  end
+
+  def self.accepted_projects(event_id)
+    accepted_registrations = process_result(event_id)
+    projects = []
+    accepted_registrations.each do |registration|
+      project = Project.find(registration.project_id)
+      accepted_projects << project
+    end
+    projects
+  end
+
+  private
+
+  def self.process_result(event_id)
+    result = get_results(event_id)
+    accepted_registrations = []
+
+    result.first(5).each do |registration_id, _|
+      process_registration(registration_id, 'accepted')
+      result.delete(registration_id)
+      accepted_registrations << find(registration_id)
+    end
+
+    result.each do |registration_id, _|
+      process_registration(registration_id, 'rejected')
+    end
+
+    accepted_registrations
+  end
+
+  def self.get_result(event_id)
+    result = {}
+
+    registration_ids_for_event.each do |registration_id|
+      votes_count = Vote.where(project_event_registration_id: registration_id).count
+      result[registration_id] = votes_count
+    end
+    result.sort_by {|_, count| -count}
+  end
+
+  def self.process_registration(registration_id, state)
+    registration = find(registration_id)
+    registration.update_attributes(state: state)
   end
 end
