630fa212b81758d499ad85804808f9060de75d9e
Dominic Wong <dominic.wong.617@gmail.com>
2014-02-26 00:57:56 +0800

    final choose button hides and shows upon request, but select2 box requires configuration for hide/show!

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -18,9 +18,6 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
-
     @user_registration_id = UserEventRegistration.find_by_user_id_and_event_id(current_user.id, params[:id]).try(:id) || 0
-    #FIXME:user's project
-    @project_registration_id = ProjectEventRegistration.find_by_project_id_and_event_id(1, params[:id]).try(:id) || 0
   end
 end
