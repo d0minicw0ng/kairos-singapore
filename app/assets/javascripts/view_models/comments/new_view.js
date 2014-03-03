28d3ba228807020b75484b11365be45adf23d977
Dominic Wong <dominic.wong.617@gmail.com>
2014-03-03 18:24:04 +0800

    Moved comments UI into view model

---
+++ app/assets/javascripts/view_models/comments/new_view.js
@@ -0,0 +1,27 @@
+function NewCommentViewModel(userId) {
+    var self = this;
+    self.userId = userId;
+    self.commentableType = $('form#new_comment').data('commentable-type');
+    self.commentableId = $('form#new_comment').data('commentable-id');
+    self.content = ko.observable();
+
+    self.submitComment = function() {
+        var data = {
+            comment: {
+                user_id: self.userId,
+                content: self.content(),
+                commentable_type: self.commentableType,
+                commentable_id: self.commentableId
+            }
+        };
+        var url = '/' + self.commentableType.toLowerCase() + 's' + '/' + self.commentableId + '/' + 'comments';
+
+        $.post(url, data, function(comment) {
+            var listItem = HandlebarsTemplates['comments/show']({
+              comment: comment
+            });
+            $('ul.comments').append(listItem);
+            $('input#comment_content').val('');
+        });
+    }
+}
