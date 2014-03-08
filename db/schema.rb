a391509535305e9a6c600c6ef65e80987fd732e0
Dominic Wong <dominic.wong.617@gmail.com>
2014-03-08 15:01:35 +0800

    Added user_id to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140306162429) do
+ActiveRecord::Schema.define(version: 20140308065829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20140306162429) do
     t.float    "latitude"
     t.float    "longitude"
     t.string   "slug"
+    t.integer  "user_id"
   end
 
   add_index "events", ["slug"], name: "index_events_on_slug", unique: true, using: :btree
