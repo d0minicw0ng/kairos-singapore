833dc7cd3cf93307ece51cc78fe9ff29768244b5
Dominic Wong <dominic.wong.617@gmail.com>
2014-03-15 14:51:37 +0800

    changing bucket for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Kairos::Application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV['S3_BUCKET_NAME'],
+      bucket: ENV['S3_PRODUCTION_BUCKET'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       s3_host_name: ENV['S3_HOSTNAME']
