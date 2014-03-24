190fa19637ac3bb5eedbf1dc1b38633da0f1d707
Dominic Wong <dominic.wong.617@gmail.com>
2014-03-24 22:43:47 +0800

    one time password generated randomly and email sent to admin

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -6,4 +6,11 @@ class ContactUsMailer < ActionMailer::Base
     mail(to: 'jian.sim@kairossociety.org', subject: data['subject'])
     mail(to: 'dominic.wong.617@gmail.com', subject: data['subject'])
   end
+
+  def new_user_one_time_password(data)
+    @email = data['email']
+    @password = data['password']
+    mail(to: 'jian.sim@kairossociety.org', subject: 'New User with one time password')
+    mail(to: 'dominic.wong.617@gmail.com', subject: 'New User with one time password')
+  end
 end
