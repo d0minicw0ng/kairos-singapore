075daebab80f31fc7c37422ceacb109e6fb2405f
Dominic Wong <dominic.wong.617@gmail.com>
2014-03-25 13:15:17 +0800

    changed Jian Min email

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -3,14 +3,14 @@ class ContactUsMailer < ActionMailer::Base
 
   def contact_us(data)
     @message = data['message']
-    mail(to: 'jian.sim@kairossociety.org', subject: data['subject'])
+    mail(to: 'sjm1988@hotmail.com', subject: data['subject'])
     mail(to: 'dominic.wong.617@gmail.com', subject: data['subject'])
   end
 
   def new_user_one_time_password(data)
     @email = data['email']
     @password = data['password']
-    mail(to: 'jian.sim@kairossociety.org', subject: 'New User with one time password')
+    mail(to: 'sjm1988@hotmail.com', subject: 'New User with one time password')
     mail(to: 'dominic.wong.617@gmail.com', subject: 'New User with one time password')
   end
 end
