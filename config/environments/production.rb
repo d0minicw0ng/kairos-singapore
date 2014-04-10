c99f70b30905aa8635310f80e272c489fd62c4a7
Dominic Wong <dominic.wong.617@gmail.com>
2014-04-10 22:39:52 +0800

    is this the case instead?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Kairos::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'kairosasean.org' }
+  config.action_mailer.default_url_options = { host: 'https://www.kairosasean.org' }
 
   # Enable threaded mode
   # config.threadsafe!
